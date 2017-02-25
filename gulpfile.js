e44a8952b7df9bde80108eac5e2555625636f87d
ALeonard9 <ALeonard9@hotmail.com>
2017-02-25 15:06:04 -0500

    Initial gulp set up

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+const jshint = require('gulp-jshint');
+
+
+gulp.task('default', function() {
+  return gulp.src('src/*.js')
+   .pipe(jshint())
+   .pipe(jshint.reporter('jshint-stylish'))
+   .pipe(jshint.reporter('fail'))
+   .pipe(concat('yokozuna.js'))
+   .pipe(uglify())
+   .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.js', ['default']);
+});
---
+++ src/user.js
@@ -0,0 +1,9 @@
+// This is a test function
+function testing() {
+  console.log('This is a test.');
+  return 'success';
+}
+
+function test() {
+  console.log('Testing again');
+}
